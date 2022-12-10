c412b71d38428af5759be72a4dd0cc41d2c6736d
DaFleks <petropoulosalex@gmail.com>
2022-12-10 18:38:16 -0500

    fixed thumbnail text link

--- components/MangaThumbnail.jsx
+++ components/MangaThumbnail.jsx
@@ -7,23 +7,26 @@ const MangaThumbnail = ({ id, idx, title, cover, count, readChapter, isChapter }
     readChapter(idx);
   };
 
+  const strShort = (str) => (str.length > 28 ? str.substr(0, 28) + "..." : str);
+
   return (
     <Grid item xs={12} sm={4} md={3}>
       <Link href={isChapter ? "javascript:void(0)" : id} onClick={isChapter ? handleCurrentChapter : () => {}}>
         <Paper variant="elevation" elevation={12} sx={{ display: "flex" }}>
-          <img src={`${MANGA_PATH}${cover}`} alt={title} width="100%" style={{ objectFit: "cover", height: "25rem" }}/>
+          <img src={`${MANGA_PATH}${cover}`} alt={title} width="100%" style={{ objectFit: "cover", height: "25rem" }} />
         </Paper>
       </Link>
 
       <Link
         underline="hover"
-        href={isChapter ? "#" : id}
+        href={isChapter ? "javascript:void(0)" : id}
+        onClick={isChapter ? handleCurrentChapter : () => {}}
         variant="body1"
         mt={2}
         ml={0.5}
         sx={{ fontWeight: "bold", color: "#FFBF00", display: "block" }}
       >
-        {title}
+        {strShort(title)}
       </Link>
       <Typography variant="body2" ml={0.5} sx={{ fontWeight: "bold", color: "#bbb" }}>
         {isChapter ? `${count} pages` : `${count} chapters`}
